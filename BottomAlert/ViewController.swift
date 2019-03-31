85073c4cbdfd37a8d6c65e9ffa27ece40e8c26e6
= <daniel.vebman@gmail.com>
2019-03-30 21:27:31 -0700

    Initial Commit

---
+++ BottomAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BottomAlert
+//
+//  Created by Daniel Vebman on 3/30/19.
+//  Copyright © 2019 Daniel Vebman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
